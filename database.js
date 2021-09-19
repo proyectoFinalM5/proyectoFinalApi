90fee589655bd97ff5ca5f62fc3e6497f84f646b
Javier Beltrán <javiierb6@gmail.com>
2021-09-18 20:06:27 -0600

    cadena de conexion a base de datos MongoDB

--- database.js
+++ database.js
@@ -1,9 +1,10 @@
-import  Mongoose  from "mongoose";
+const  Mongoose  =require("mongoose");
 
 Mongoose
 //.connect("mongodb://localhost/emprendeapp")
 .connect("mongodb+srv://javi:1234@comercios.yd3gh.mongodb.net/emprendeapp?retryWrites=true&w=majority")
-.then((db) => console.log("Ya en linea XD"))
-.catch((err) => console.log("No se conecta :( "));
+.then((db)=>console.log("Ya en linea XD"))
+.catch((err)=>console.log("No se conecta :("));
 
-export default Mongoose
+
+module.exports= Mongoose
