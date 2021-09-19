ff4c4cccfa04533e2865fe769a5ea47a3c5509ff
Javier Beltrán <54746606+javy10@users.noreply.github.com>
2021-09-19 10:36:51 -0600

    modelo Comercio(#4)
    * schema de modeloComercio
    
    * carpeta helpers

--- src/models/comercio.js
+++ src/models/comercio.js
@@ -1,6 +1,16 @@
 import { Schema, model } from 'mongoose'
-const schema = Schema({
-    nombre: { type: String, required: true },
-    direccion: { type: String, required: true }
+const comercioSchema = Schema({
+    nombre_comercio: { type: String, max: 15, min: 5, required: true },
+    nombre_dueño: { type: String, max: 15, min: 5, required: true },
+    //localizacion: { type: location, require: true },
+    localizacion: { type: location, coordinates: [ x, y ] },
+    telefono: { type: Number, max: 8, min:8, require: true },
+    redes_sociales: {type: String, min:8 },
+    categoria: { type: String, min:4, require: true },
+    descripcion: { type: String, min: 5, max: 100, require: true },
+    logo: { type: String, min: 5, require: true }
 })
-export default model('Comercio', schema)
+
+
+
+export default model('Comercio', comercioSchema)
