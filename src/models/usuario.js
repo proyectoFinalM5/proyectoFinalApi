f6bb2290a9d3866e155fd67173ca17af74453fe3
giovaRamos <rj0215032016@unab.edu.sv>
2021-09-20 14:30:21 -0600

    Usuario alterad0

--- src/models/usuario.js
+++ src/models/usuario.js
@@ -1,8 +1,11 @@
 
 import { Schema, model } from 'mongoose'
 const usuarioSchema = Schema({
-    nombre_usuario: { type: String, max: 15, min: 5, required: true },
+    nombre: { type: String, max: 15, min: 5, required: true },
+    apellido:{ type: String, max: 15, min: 5, required: true },
     telefono: { type: Number, max: 8, min:8, require: true },
+    email: { type: String, min: 5, max: 100, require: true },
+    password: { type: String, max: 15, min: 5, required: true },
     rol: { type: String, min:4, require: true },
 })
 
