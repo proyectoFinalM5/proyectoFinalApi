049e5db97622f568fe8a2a80acd93afdbfb79224
byronescobar <57452296+byronescobar@users.noreply.github.com>
2021-09-20 14:47:44 -0600

    inicio swagger (#10)
    Co-authored-by: byron <byronalexcasto@gmail.com>

--- src/app.js
+++ src/app.js
@@ -1,8 +1,25 @@
 import Express from 'express'
 import Morgan from 'morgan'
 import RutasComercio from './routes/comercio.routes.js'
+import swaggerJsDoc from 'swagger-jsdoc'
+import swaggerUI from 'swagger-ui-express'
+
+const swaggerOptions = {
+    swaggerDefinition: {
+      info: {
+        title: "Comercios API",
+        version: '1.0.0',
+      },
+    },
+    apis: ["./routes/comercio.routes.js","./routes/usuario.routes.js"],
+};
+  
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
 
 const app = Express();
+app.use('/Comercios-doc', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
+
 app.use(Express.json());
 app.use(Express.urlencoded({ extended: true }));
 app.use(Morgan('dev'))
