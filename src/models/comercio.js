5851d208ec58eb01965f122bed853dde6729ed68
Javier Beltrán <javiierb6@gmail.com>
2021-09-19 19:35:49 -0600

    cambios en el models de comercio

--- src/models/comercio.js
+++ src/models/comercio.js
@@ -1,9 +1,19 @@
-import { Schema, model } from 'mongoose'
-const comercioSchema = Schema({
-    nombre_comercio: { type: String, max: 15, min: 5, required: true },
-    nombre_dueño: { type: String, max: 15, min: 5, required: true },
-    //localizacion: { type: location, require: true },
-    localizacion: { type: location, coordinates: [ x, y ] },
+import mongoose from 'mongoose'
+
+const comercioSchema = mongoose.Schema({
+    comercio: { type: String, max: 15, min: 5, required: true },
+    propietario: { type: String, max: 15, min: 5, required: true },
+    location: {
+        type: {
+            type: String, // Don't do `{ location: { type: String } }`
+            enum: ['Point'], // 'location.type' must be 'Point'
+            required: true
+        },
+        coordinates: {
+            type: [Number],
+            required: true
+        }
+    },
     telefono: { type: Number, max: 8, min:8, require: true },
     redes_sociales: {type: String, min:8 },
     categoria: { type: String, min:4, require: true },
@@ -11,4 +21,4 @@ const comercioSchema = Schema({
     logo: { type: String, min: 5, require: true }
 })
 
-export default model('Comercio', comercioSchema)
+export default mongoose.model('Comercio', comercioSchema)
